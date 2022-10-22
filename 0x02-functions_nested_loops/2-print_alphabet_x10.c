6324d57bce679dfb565a851cf36cf35807cc39eb
VonettaOrinda <thevonetteway@gmail.com>
2022-10-22 05:32:07 -0700

    task 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alaphabet - prints the alphabet in lowercase,followed by a new line.
+ */
+i
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
