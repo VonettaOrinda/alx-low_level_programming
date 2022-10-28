333bdaaea9ad72560210eaae0dc0887967075a5d
VONETTA <thevonetteway@gmail.com>
2022-10-28 12:30:20 +0300

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ *
+ * @n: integer value
+ *
+ * Return: success
+ */
+
+int _abs(int n)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
