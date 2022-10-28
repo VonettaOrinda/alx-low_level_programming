c286d21c11d668e28b83ab740af111ae148710df
VONETTA <thevonetteway@gmail.com>
2022-10-28 14:27:50 +0300

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *string_toupper - convert string to uppercase
+ *
+ * @str: string to be converted
+ * Return: char*
+ */
+
+char *string_toupper(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = (int)str[i] - 32;
+}
+}
+return (str);
+}
